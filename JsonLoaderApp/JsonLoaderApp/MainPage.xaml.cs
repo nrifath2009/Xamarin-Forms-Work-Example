69dbb849e83793ea87b0054ae37c59238cadff54
nrifath2009 <nrifath2009@gmail.com>
2018-11-05 02:49:41 -0600

    Initial Commit

--- JsonLoaderApp/JsonLoaderApp/MainPage.xaml.cs
+++ JsonLoaderApp/JsonLoaderApp/MainPage.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -12,6 +16,19 @@ namespace JsonLoaderApp
         public MainPage()
         {
             InitializeComponent();
+            LoadData();
+        }
+        private void LoadData()
+        {
+            string personListText = string.Empty;
+            var assembly = typeof(MainPage).GetTypeInfo().Assembly;
+
+            using (var stream = assembly.GetManifestResourceStream("JsonLoaderApp.DataService.data.json"))
+            {
+                personListText = new StreamReader(stream).ReadToEnd();
+            }
+            var personList = JsonConvert.DeserializeObject<ObservableCollection<Person>>(personListText);
+            personListView.ItemsSource = personList;
         }
     }
 }
---
+++ JsonLoaderApp/JsonLoaderApp/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonLoaderApp
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Company { get; set; }
+
+    }
+}
